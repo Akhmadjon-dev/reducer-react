42f69570541667311c4e371891829a214ab4efc0
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-03-02 19:53:47 +0500

    readucer example created for react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
+import "./styles.css";
+import reducer, { ADD_TODO, REMOVE_TODO, COMPLETE_TODO } from "./store/reducer";
 
-function App() {
+export default function App() {
+  const [id, setId] = useState(0);
+  const [text, setText] = useState("");
+  const initialState = [
+    {
+      id: id,
+      text: "First Item",
+      completed: false
+    }
+  ];
+
+  //We could also pass an empty array as the initial state
+  //const initialState = []
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const addTodoItem = (e) => {
+    e.preventDefault();
+    const newId = id + 1;
+    setId(newId);
+    dispatch({
+      type: ADD_TODO,
+      id: newId,
+      text: text
+    });
+    setText("");
+  };
+  const removeTodo = (id) => {
+    dispatch({ type: REMOVE_TODO, id });
+  };
+  const completeTodo = (id) => {
+    dispatch({ type: COMPLETE_TODO, id });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo Example</h1>
+      <form className="input" onSubmit={addTodoItem}>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button disabled={text.length === 0} type="submit">+</button>
+      </form>
+      <div className="todos">
+        {state.map((todo) => (
+          <div key={todo.id} className="todoItem">
+            <p className={todo.completed && "strikethrough"}>{todo.text}</p>
+            <span onClick={() => removeTodo(todo.id)}>✕</span>
+            <span onClick={() => completeTodo(todo.id)}>✓</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
